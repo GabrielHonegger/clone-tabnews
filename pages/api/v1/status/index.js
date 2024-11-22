c04e6eff21d0356a95d913a2bba6e5108eb35745
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-11-22 17:58:01 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 5 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json("alunos de curso.dev são pessoas acima da média");
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await database.query("SHOW server_version;");
+  const postgresVersion = versionResult.rows[0].server_version;
+
+  const maxResult = await database.query("SHOW max_connections;");
+  const maxConnections = await maxResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = openedResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+        version: postgresVersion,
+      },
+    },
+  });
 }
 
 export default status;
